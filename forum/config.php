9c4a0246cac576b356ee061f258d85f95f11cb1b
Panagiotis Moustafellos <pmoust@gmail.com>
2014-01-25 16:42:56 +0100

    db pass

---
+++ forum/config.php
@@ -0,0 +1,16 @@
+<?php
+// phpBB 3.0.x auto-generated configuration file
+// Do not change anything in this file!
+$dbms = 'mysqli';
+$dbhost = '127.0.0.1';
+$dbport = '';
+$dbname = 'companera';
+$dbuser = 'companera';
+$dbpasswd = get_cfg_var('companera_password');
+$table_prefix = 'companera_';
+$acm_type = 'file';
+$load_extensions = '';
+
+@define('PHPBB_INSTALLED', true);
+// @define('DEBUG', true);
+// @define('DEBUG_EXTRA', true);
